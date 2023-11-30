79d0c0f90472b2b8514acc21aafe26dd44084366
mateusbrandao <mateus.brandaoteixeira@gmail.com>
2023-11-29 21:01:00 -0300

    adicionando logica de transacoes

--- src/main/java/ttpe/projeto/service/GerenciadorEstoqueService.java
+++ src/main/java/ttpe/projeto/service/GerenciadorEstoqueService.java
@@ -14,8 +14,7 @@ public class GerenciadorEstoqueService {
 	public boolean registrarTransacao(String idProduto, int quantidade, String tipoTransacao, Empresa empresaOrigem,
 			Empresa empresaDestino, Estoque estoque) throws Exception {
 		try {
-			Produto produto;
-			produto = estoque.getProdutoPorId(idProduto);
+			Produto produto = estoque.getProdutoPorId(idProduto);
 			if (produto == null) {
 				throw new Exception("Produto não encontrado.");
 			}
@@ -24,12 +23,62 @@ public class GerenciadorEstoqueService {
 			if (transacao.getQuantidade() < 0) {
 				throw new ValorInvalidoException("quantidade transacao inválida.");
 			}
-			estoque.adicionarTransacao(transacao);	
+			
+			estoque.adicionarTransacao(transacao);
+
+			switch (tipoTransacao) {
+			case "Venda":
+				transacaoVendaProduto(produto, quantidade);
+				break;
+			case "Recebimento":
+				transacaoRecebimentoProduto(produto, quantidade);
+				break;
+			case "Devolução":
+				transacaoDevolucaooProduto(produto, quantidade);
+				break;
+			case "Transferencia":
+				transacaoTransferenciaProduto(produto, empresaOrigem, empresaDestino, quantidade);
+				break;
+			default:
+
+				System.out.println("Tipo de transação não reconhecido: " + tipoTransacao);
+				break;
+			}
 			return true;
 		} catch (ProdutoInvalidoException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
 		}
+	}
+	public void transacaoVendaProduto(Produto produto, int quantidade) {
+		produto.removerEstoque(quantidade);
+	}
+
+	public void transacaoRecebimentoProduto(Produto produto, int quantidade) {
+		produto.adicionarEstoque(quantidade);
+	}
+
+	public void transacaoDevolucaooProduto(Produto produto, int quantidade) {
+		produto.adicionarEstoque(quantidade);
+	}
+
+	public void transacaoTransferenciaProduto(Produto produto, Empresa empresaOrigem, Empresa empresaDestino,
+			int quantidade) throws Exception {
+
+		if (produto == null) {
+			throw new Exception("Produto não encontrado.");
+		}
+
+		if (produto.getEmpresaDetentora().getId() != empresaOrigem.getId()) {
+			throw new Exception("O produto não pertence à empresa de origem.");
+		}
+
+		if (produto.getQuantidadeEmEstoque() < quantidade) {
+			throw new Exception("Quantidade em estoque insuficiente para transferência.");
+		}
+
+		produto.setEmpresaDetentora(empresaDestino);
+
 	}
 }
