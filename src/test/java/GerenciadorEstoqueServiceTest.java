0123f99ecd3dab6f1802c6f5a6f493f52fcc695d
mateusbrandao <mateus.brandaoteixeira@gmail.com>
2023-11-29 21:39:01 -0300

    adicionando test parametrizado resultado transacao

--- src/test/java/GerenciadorEstoqueServiceTest.java
+++ src/test/java/GerenciadorEstoqueServiceTest.java
@@ -21,7 +21,7 @@ public class GerenciadorEstoqueServiceTest {
 	private String descricaoPadrao = "Parafuso Phillips 6Mm X 41Mm";
 	private String codigoBarraPadrao = "00025163";
 	private double precoPadrao = 10.5;
-	private int qtdAtualPadrao = 5;
+	private int qtdAtualPadrao = 100;
 	private Empresa empresaPadrao = new Empresa(1, "35737451000190", "MATRIZ", "rua portugal");
 	private Fornecedor fornecedorPadrao = new Fornecedor(1, "fornecedor test", "6198460585", "endereço teste");
 	private int qtdMinimaPadrao = 30;
@@ -68,6 +68,27 @@ public class GerenciadorEstoqueServiceTest {
 	        estoque.adicionarProduto(produtoMock);
 	        boolean resultado = service.registrarTransacao(idProduto, quantidade, tipoTransacao, empresaPadrao, empresaDestino, estoque);
 	        Assertions.assertEquals(resultadoEsperado, resultado);
+	    }
+	    
+	    private static Stream<Arguments> registrarTransacaoResultadoArguments() {
+	        return Stream.of(
+	            Arguments.of("1", 5, "Venda", 95),
+	            Arguments.of("1", 5, "Recebimento", 105),
+	            Arguments.of("1", 10, "Devolução", 110)
+	           
+	        );
+	    }
+
+	    @ParameterizedTest
+	    @MethodSource("registrarTransacaoResultadoArguments")
+	    void testRegistrarTransacaoResultadoParametrizado(String idProduto, int quantidade, String tipoTransacao, int resultadoEsperado) throws Exception {
+	        Produto produtoMock = new Produto(id, nomePadrao, descricaoPadrao, codigoBarraPadrao, precoPadrao,
+					qtdAtualPadrao, empresaPadrao, fornecedorPadrao, qtdMinimaPadrao, dataAtualPadrao);
+	        Estoque estoque  = new Estoque();
+	        Empresa empresaDestino = new Empresa(2, "35737451000290", "FILIAL", "rua portugal");
+	        estoque.adicionarProduto(produtoMock);
+	        service.registrarTransacao(idProduto, quantidade, tipoTransacao, empresaPadrao, empresaDestino, estoque);
+	        Assertions.assertEquals(resultadoEsperado, produtoMock.getQuantidadeEmEstoque());
 	    }
 	
 }
